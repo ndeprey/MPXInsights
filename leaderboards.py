b28f1855dc320a04ba0050a0f5251412491cc1ca
Nicholas DePrey <developer@dev-ndeprey.npr.org>
2015-08-19 18:41:18 -0400

    Changed to percentage of plays instead of plays and much cleanup

--- leaderboards.py
+++ leaderboards.py
@@ -2,12 +2,14 @@ import MySQLdb
 import pandas as pd
 import requests
 
+pd.set_option('display.max_colwidth',100)
+
 con = MySQLdb.connect('stage4-mysql.npr.org',
 		      'admin',
 		      'admin',
 		      'infinite')
 
-sql = ('SELECT ratings_story_id, count(ratings_rating) as ThumbupsAndShares '
+sql1 = ('SELECT ratings_story_id, count(ratings_rating) as ThumbupsAndShares '
 	'FROM infinite.user_ratings '
 	'WHERE ratings_rating in %(ratings)s '
 	'AND ratings_origin in %(origins)s '
@@ -16,10 +18,37 @@ sql = ('SELECT ratings_story_id, count(ratings_rating) as ThumbupsAndShares '
 	'ORDER By ThumbupsAndShares desc '
 	'limit 1000')
 
-params = {"ratings": ['THUMBUP','SHARE'],
+params1 = {"ratings": ['THUMBUP','SHARE'],
 	"origins": ['LEAD','CORE','BREAK','SELECTS','INVEST','EDTR','MUSICINT','OPENDOOR'] }
 
-df = pd.read_sql(sql, con=con, params=params)
+df = pd.read_sql(sql1, con=con, params=params1)
+
+sql2 = ('SELECT ratings_story_id, count(ratings_rating) as totalPlays '
+        'FROM infinite.user_ratings '
+        'WHERE ratings_rating in %(ratings)s '
+        'AND ratings_origin in %(origins)s '
+        'and ratings_timestamp >= Now() - interval "24" HOUR '
+        'GROUP BY ratings_story_id '
+        'ORDER By totalPlays desc '
+        'limit 1000')
+
+params2 = {"ratings": ['THUMBUP','SHARE','COMPLETED','SKIP','START'],
+        "origins": ['LEAD','CORE','BREAK','SELECTS','INVEST','EDTR','MUSICINT','OPENDOOR'] }
+
+df2 = pd.read_sql(sql2, con=con, params=params2)
+
+df = pd.merge(df, df2,
+	    left_on = ['ratings_story_id'],
+	    right_on = ['ratings_story_id'],
+	    how = 'inner')
+
+df['Thumbup_Share_pct'] = df['ThumbupsAndShares'] / df['totalPlays']
+
+df['Thumbup_Share_pct'] = [100*round(i,4) for i in df['Thumbup_Share_pct']]
+
+df = df[df['totalPlays'] >= 200]
+
+df = df.sort(['Thumbup_Share_pct'], ascending=False)
 
 print 'loaded results from SQL query with rows = ', len(df)
 con.close()
@@ -49,7 +78,9 @@ df.to_csv('/home/developer/leaderboards/newsmag_leaderboard.csv', encoding='utf-
 
 print 'Script completed'
 
-dfhtml = df.to_html().replace('<table border="1" class="dataframe">','<table class="table table-striped">') # use bootstrap styling
+df = df[['ratings_story_id','title','Thumbup_Share_pct']]
+
+dfhtml = df.to_html(index=False).replace('<table border="1" class="dataframe">','<table class="table table-striped">') # use bootstrap styling
 
 html_string = '''
 <html>
@@ -59,6 +90,7 @@ html_string = '''
     </head>
     <body>
        <h1>NPR One Leaderboard</h1>
+	  <p>A list of the 10 most frequently Liked and Shared pieces among New Magazine content in the last 24 hours</p>
 ''' + dfhtml + '''
     </body>
 </html>'''
