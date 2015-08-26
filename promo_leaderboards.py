7c37129a5aaa13b9554043e9f5c803b8eba97fec
Nicholas DePrey <developer@dev-ndeprey.npr.org>
2015-08-26 12:53:32 -0400

    initial commit of promos leaderboard

--- promo_leaderboards.py
+++ promo_leaderboards.py
@@ -19,31 +19,31 @@ con = MySQLdb.connect(host=cfg['Cobalt']['host'],
                         db=cfg['Cobalt']['db']
                      )
 
-sql1 = ('SELECT ratings_story_id, count(ratings_rating) as ThumbupsAndShares '
+sql1 = ('SELECT ratings_story_id, count(ratings_rating) as Tapthroughs '
 	'FROM infinite.user_ratings '
 	'WHERE ratings_rating in %(ratings)s '
 	'AND ratings_origin in %(origins)s '
 	'and ratings_timestamp >= Now() - interval "48" HOUR '
 	'GROUP BY ratings_story_id '
-	'ORDER By ThumbupsAndShares desc '
+	'ORDER By Tapthroughs desc '
 	'limit 1000')
 
-params1 = {"ratings": ['THUMBUP','SHARE'],
-	"origins": ['LEAD','CORE','BREAK','SELECTS','INVEST','EDTR','MUSICINT','OPENDOOR'] }
+params1 = {"ratings": ['TAPTHRU'],
+	"origins": ['FTRPROMO'] }
 
 df = pd.read_sql(sql1, con=con, params=params1)
 
-sql2 = ('SELECT ratings_story_id, count(ratings_rating) as totalPlays '
+sql2 = ('SELECT ratings_story_id, count(ratings_rating) as totalImpressions '
         'FROM infinite.user_ratings '
         'WHERE ratings_rating in %(ratings)s '
         'AND ratings_origin in %(origins)s '
         'and ratings_timestamp >= Now() - interval "48" HOUR '
         'GROUP BY ratings_story_id '
-        'ORDER By totalPlays desc '
+        'ORDER By totalImpressions desc '
         'limit 1000')
 
-params2 = {"ratings": ['THUMBUP','SHARE','COMPLETED','SKIP','START'],
-        "origins": ['LEAD','CORE','BREAK','SELECTS','INVEST','EDTR','MUSICINT','OPENDOOR'] }
+params2 = {"ratings": ['THUMBUP','SHARE','COMPLETED','SKIP','START','TAPTHRU'],
+        "origins": ['FTRPROMO'] }
 
 df2 = pd.read_sql(sql2, con=con, params=params2)
 
@@ -52,13 +52,13 @@ df = pd.merge(df, df2,
 	    right_on = ['ratings_story_id'],
 	    how = 'inner')
 
-df['Thumbup_Share_pct'] = df['ThumbupsAndShares'] / df['totalPlays']
+df['Tapthrough_rate'] = df['Tapthroughs'] / df['totalImpressions']
 
-df['Thumbup_Share_pct'] = [100*round(i,4) for i in df['Thumbup_Share_pct']]
+df['Tapthrough_rate'] = [100*round(i,4) for i in df['Tapthrough_rate']]
 
-df = df[df['totalPlays'] >= 1000]
+# df = df[df['totalImpressions'] >= 1000]
 
-df = df.sort(['Thumbup_Share_pct'], ascending=False)
+df = df.sort(['Tapthrough_rate'], ascending=False)
 
 print 'loaded results from SQL query with rows = ', len(df)
 con.close()
@@ -76,14 +76,17 @@ def get_title(id):
 	'apiKey':'MDE1MTM2Mjk1MDE0MDUwMTIwNzlmYTdkNA001'}
 	r = requests.get(baseurl, params=payload)
 	r = r.json()
-	title = r['list']['story'][0]['title']['$text']
+	try:
+		title = r['list']['story'][0]['title']['$text']
+	except:
+		title = '(no title found)'
 	return title
 
 df['title'] = [get_title(id) for id in df['ratings_story_id']]
 
 print 'done fetching titles'
 
-df.to_csv('/var/www/newsmag_leaderboard.csv', encoding='utf-8', index=False)
+df.to_csv('/var/www/promo_leaderboard.csv', encoding='utf-8', index=False)
 
 df = df[0:10]
 
@@ -91,7 +94,7 @@ df['link_story_id'] = ['<a href="http://npr.org/' + str(int(i)) + '">' + str(int
 
 print 'Script completed'
 
-df = df[['link_story_id','title','Thumbup_Share_pct']]
+df = df[['link_story_id','title','totalImpressions','Tapthrough_rate']]
 
 dfhtml = df.to_html(index=False,escape=False).replace('<table border="1" class="dataframe">','<table class="table table-striped">') # use bootstrap styling
 
@@ -103,18 +106,19 @@ html_string = '''
 	<meta http-equiv="refresh" content="300" >
     </head>
     <body>
-	 <h1>NPR One Leaderboard</h1>
+	 <h1>NPR One Promo Leaderboard</h1>
 	 <h5>Last Updated: '''+ st + '''</h5>
-	  <p>A list of the 10 most frequently Liked and Shared pieces among New Magazine content in NPR One over the last 48 hours. Minimum plays = 1000</p>
+	  <p>A list of the 10 promos with the highest tapthrough rates in NPR One over the last 48 hours.</p>
 ''' + dfhtml + '''
     </body>
 </html>'''
 
-f = open('/var/www/news_mag_leaderboard.html','w')
+f = open('/var/www/promo_leaderboard.html','w')
 f.write(html_string)
 f.close()
 
 print 'done saving HTML table'
+
 
 
 
